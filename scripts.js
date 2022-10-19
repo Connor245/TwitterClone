54d936dce55e372c946a153d89400fe74b9967a0
Connor245 <pass4github>
2022-10-19 13:56:25 -0400

    updates before submit

--- scripts.js
+++ scripts.js
@@ -4,13 +4,13 @@ function processForm(e) {
 
     //store the thibng that was typed
     const tweet = document.getElementById('tweet');
-
+    if(tweet.value =="") return false;
     //show the tweet in our timeline
     const ul = document.getElementById('timeline');
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(tweet.value));
     ul.appendChild(li);
-
+    tweet.value = ""
     return false;
 }
 
